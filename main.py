ec5646ef1e065ea817caab32ea98bf8038004e30
sptryogi <115679084+sptryogi@users.noreply.github.com>
2025-10-28 10:57:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -168,6 +168,17 @@ def process_rekap(order_df, income_df, seller_conv_df, service_fee_df):
                         part_to_append = paket_match.group(1) # Hasilnya 'PAKET ISI 7'
                     elif satuan_match:
                         part_to_append = 'SATUAN'
+                    elif:
+                        # --- LOGIKA FALLBACK TAMBAHAN ---
+                        # Jika bukan PAKET/SATUAN, jalankan logika generik
+                        if ',' in var_str:
+                            parts = [p.strip().upper() for p in var_str.split(',')]
+                            size_keywords = {'QPP', 'A5', 'B5', 'A6', 'A7', 'HVS', 'KORAN'}
+                            relevant_parts = [p for p in parts if p in size_keywords]
+                            if relevant_parts:
+                                part_to_append = relevant_parts[0]
+                        else:
+                            part_to_append = var_str'
                     else:
                         # --- TAMBAHAN ---
                         # Biarkan part_to_append kosong agar dicek logika di bawah
