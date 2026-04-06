3cebf52b052b87c4bba31033f9dcbb47da31ad6a
sptryogi <115679084+sptryogi@users.noreply.github.com>
2026-04-06 16:39:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3761,7 +3761,7 @@ if marketplace_choice:
     # Konfigurasi file opsional per toko untuk TikTok
     OPTIONAL_FILES_TIKTOK = {
         'creator_order': ['Raka Bookstore', 'Toko Kaliba'],
-        'product_data': ['Raka Bookstore', 'Toko Kaliba', 'Human Store'],
+        'product_data': ['Raka Bookstore', 'Toko Kaliba', 'Human Store', 'Pacific Bookstore', 'DAMA.ID STORE'],
         'pdf_resi': ['Raka Bookstore', 'Toko Kaliba', 'Human Store']  # Jika juga ingin PDF opsional
     }
     
